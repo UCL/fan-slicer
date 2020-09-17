a267e1868bdcf5243138267dbc40d8a522016d4c
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-09-17 14:55:19 +0100

    Some linting.

--- tests/test_susi.py
+++ tests/test_susi.py
@@ -6,6 +6,7 @@ from susi.ui.susi_demo import run_demo
 from susi.algorithms import addition, multiplication
 
 # Pytest style
+
 def test_using_pytest_susi():
     """First test"""
     #pylint:disable=invalid-name
@@ -17,9 +18,11 @@ def test_using_pytest_susi():
     expected_answer = 3
     assert run_demo(x, y, multiply, verbose) == expected_answer
 
+
 def test_addition():
     """ Test addition """
     assert addition.add_two_numbers(1, 2) == 3
+
 
 def test_multiplication():
     """ Test multiplication """
