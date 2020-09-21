aa52c119f922f8e88d34449ad1d1a4233728c9cc
Joao Ramalhinho <joao.ramalhinho.15@ucl.ac.uk>
2020-09-21 17:13:55 +0100

    Issue #1: Added test for simulation of 2D CT projections.

--- tests/test_pycuda_simulations.py
+++ tests/test_pycuda_simulations.py
@@ -85,5 +85,52 @@ def test_binary_image_sim():
                                          decimal=7)
 
 
+def test_intensity_image_sim():
+    """
+    Test intensity image simulation
+    THIS TEST NEEDS PYCUDA
+    """
+    # Set config
+    config_dir = "config/models_intensity_LUS_config.json"
+    vol_dir = "data_LUS/CT_Dicom/000/"
+    # Load the segmented volume
+    ct_volume = ivol.IntensityVolume(config_dir=config_dir,
+                                     vol_dir=vol_dir,
+                                     file_type="dicom")
+
+    # Define test poses
+    pose1 = np.array([[-0.46,	0.29, -0.84	, -41.77],
+                      [0.28, 0.94, 0.18, -47.72],
+                      [0.84, -0.15, -0.52, -253.44],
+                      [0, 0, 0, 1]])
+    pose2 = np.array([[-0.46,	0.29, -0.84	, -41.77],
+                      [0.28, 0.94, 0.18, -47.72],
+                      [0.84, -0.15, -0.52, -273.24],
+                      [0, 0, 0, 1]])
+    test_poses = np.hstack((pose1, pose2))
+
+    # Load expected image (as array, as the bounds are not
+    # between 0 and 255 or 0 and 1)
+    image1 = np.load('data_tests/intensity_liver_image_0.npy')
+    image2 = np.load('data_tests/intensity_liver_image_1.npy')
+
+    # Simulate images
+    _, ct_map = \
+        ct_volume.simulate_image(poses=test_poses,
+                                 downsampling=2,
+                                 image_num=2)
+
+    # Assert the two images
+    # Due to numerical error in saving images,
+    # accept tolerance of 10^-3
+    test_image1 = ct_map[:, :, 0]
+    np.testing.assert_array_almost_equal(test_image1,
+                                         image1,
+                                         decimal=3)
+    test_image2 = ct_map[:, :, 1]
+    np.testing.assert_array_almost_equal(test_image2,
+                                         image2,
+                                         decimal=3)
+
 
 
