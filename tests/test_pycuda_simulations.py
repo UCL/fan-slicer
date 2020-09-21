10225a12650423d9d9c90925e0f35a58a8d9de6e
Joao Ramalhinho <joao.ramalhinho.15@ucl.ac.uk>
2020-09-21 16:08:55 +0100

    Issue #1: Added test for binary image simulation.

--- tests/test_pycuda_simulations.py
+++ tests/test_pycuda_simulations.py
@@ -4,12 +4,14 @@
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 import susi.pycuda_simulation.mesh as mesh
 import susi.pycuda_simulation.segmented_volume as svol
 import susi.pycuda_simulation.intensity_volume as ivol
 
 # These tests access the data in data_LUS and data_EUS
 # used in the demo scripts
+
 
 def test_voxelisation():
     """
@@ -40,6 +42,7 @@ def test_voxelisation():
 def test_binary_image_sim():
     """
     Test binary image simulation
+    THIS TEST NEEDS PYCUDA
     """
     # Set config
     config_dir = "config/models_binary_LUS_config.json"
@@ -48,7 +51,38 @@ def test_binary_image_sim():
     liver_volume = svol.SegmentedVolume(config_dir=config_dir,
                                         mesh_dir=mesh_dir,
                                         voxel_size=0.5)
-    assert 0==0
+    # Define test poses
+    pose1 = np.array([[-0.46,	0.29, -0.84	, -41.77],
+                      [0.28, 0.94, 0.18, -47.72],
+                      [0.84, -0.15, -0.52, -253.44],
+                      [0, 0, 0, 1]])
+    pose2 = np.array([[-0.46,	0.29, -0.84	, -41.77],
+                      [0.28, 0.94, 0.18, -47.72],
+                      [0.84, -0.15, -0.52, -273.24],
+                      [0, 0, 0, 1]])
+    test_poses = np.hstack((pose1, pose2))
+
+    # Load expected image (an extra channel is added, be careful to remove)
+    image1 = plt.imread('data_tests/binary_liver_image_0.png', format='png')
+    image2 = plt.imread('data_tests/binary_liver_image_1.png', format='png')
+
+    # Simulate images
+    _, _, colored_map = \
+        liver_volume.simulate_image(poses=test_poses,
+                                    downsampling=2,
+                                    image_num=2)
+
+    # Assert the two images
+    # Due to numerical error in saving images,
+    # accept tolerance of 10^-7
+    test_image1 = colored_map[:, :, :, 0]
+    np.testing.assert_array_almost_equal(test_image1,
+                                         image1[:, :, 0:3],
+                                         decimal=7)
+    test_image2 = colored_map[:, :, :, 1]
+    np.testing.assert_array_almost_equal(test_image2,
+                                         image2[:, :, 0:3],
+                                         decimal=7)
 
 
 
