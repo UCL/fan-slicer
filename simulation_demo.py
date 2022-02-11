56fa9816ecd3ad4702824b7d895a2ec8c5a67d43
Joao Ramalhinho <joao.ramalhinho.15@ucl.ac.uk>
2022-02-11 16:09:11 +0000

    Refactored demo script.

--- simulation_demo.py
+++ simulation_demo.py
@@ -1,9 +1,8 @@
 # coding=utf-8
 
 """
-A demo script that shows simulation of Laparoscopic
-Ultrasound planes in both segmented and original CT
-data.
+A demo script to illustrate simulation of segmented and intensity images with
+ultrasound image shapes.
 """
 
 
@@ -18,35 +17,34 @@ liver_volume = svol.SegmentedVolume(config_dir="config/models_binary_LUS_config.
                                     mesh_dir="data/data_LUS/",
                                     voxel_size=0.5,
                                     downsampling=2,
-                                    image_num=100)
+                                    image_num=10)
 # Load the intensity volume
 ct_volume = ivol.IntensityVolume(config_dir="config/models_intensity_LUS_config.json",
                                  vol_dir="data/data_LUS/CT_test_volume.npy",
                                  file_type="npy",
                                  npy_config="config/CT_npy_volume_config.json",
                                  downsampling=2,
-                                 image_num=100)
+                                 image_num=10)
 
-# Define a set of 100 poses, and slice generated volumes
+# Define a set of 10 poses, and slice generated volumes
 pose = np.array([[-0.46,	0.29, -0.84	, -41.77],
                  [0.28, 0.94, 0.18, -47.72],
                  [0.84, -0.15, -0.52, -253.44],
                  [0, 0, 0, 1]])
 
-poses = np.tile(pose, (1, 100))
-poses[2, 3:400:4] = poses[2, 3:400:4] - np.arange(0, 20, 0.2)
+poses = np.tile(pose, (1, 10))
+poses[2, 3:40:4] = poses[2, 3:40:4] - np.arange(0, 20, 2)
 
 # Simulate binary maps, always matching image_num with the number of poses
-# And using downsampling to reduce image dimensions
 points, binary_map, colored_map = \
-    liver_volume.simulate_image(poses=poses, image_num=100)
+    liver_volume.simulate_image(poses=poses, image_num=10)
 
 points, ct_map = \
-    ct_volume.simulate_image(poses=poses, image_num=100)
+    ct_volume.simulate_image(poses=poses, image_num=10)
 
 
 # Show results image by image
-for i in range(100):
+for i in range(10):
     fig = plt.figure()
     plt.clf()
     ax1 = fig.add_subplot(121)
